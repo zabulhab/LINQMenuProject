a2eb560c794b2b9ed711ab07652ec13f26690f0c
Zena Abulhab <zabulhab@umich.edu>
2020-10-27 01:59:47 -0400

    Deleted previous db's migrations files and built db again

---
+++ Migrations/20201027055828_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFTest.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false),
+                    IsMeat = table.Column<bool>(nullable: false),
+                    IsNut = table.Column<bool>(nullable: false),
+                    HasDairy = table.Column<bool>(nullable: false),
+                    HasGluten = table.Column<bool>(nullable: false),
+                    IsSpicy = table.Column<bool>(nullable: false),
+                    DishID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Name);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_Dishes_DishID",
+                        column: x => x.DishID,
+                        principalTable: "Dishes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_DishID",
+                table: "Ingredients",
+                column: "DishID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Dishes");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,61 +11,57 @@ namespace EFTest
     {
         static void Main()
         {
-            using (var db = new MenuContext())
+            using var db = new MenuContext();
+            Console.WriteLine("Inserting a salad into dishes table");
+            db.Add(new Dish { Name = "Salad", ID = 01 });
+
+            Console.WriteLine("Inserting 5 different ingredients into ingredient table");
+            db.Add(new Ingredient { Name = "Apple" });
+            db.Add(new Ingredient { Name = "Sugar" });
+            db.Add(new Ingredient { Name = "Flour", HasGluten = true });
+            db.Add(new Ingredient { Name = "Butter", HasDairy = true });
+            db.Add(new Ingredient { Name = "Beef", IsMeat = true });
+
+
+            Console.WriteLine("Inserting apple and meat pies into dishes table");
+            db.Add(new Dish { Name = "Apple Pie", ID = 02 });
+            db.Add(new Dish { Name = "Meat Pie", ID = 03 });
+
+            db.SaveChanges();
+
+            var applePie = db.Dishes.First(d => d.ID == 02);
+            // store apple pie ingredients by retrieving them from ingredients db
+            Console.WriteLine("Storing the ingredients for dish");
+            applePie.Ingredients.Add(
+                db.Ingredients.First(i => i.Name == "Apple")
+                );
+            applePie.Ingredients.Add(
+                db.Ingredients.First(i => i.Name == "Sugar")
+                );
+            applePie.Ingredients.Add(
+                db.Ingredients.First(i => i.Name == "Flour")
+            );
+            applePie.Ingredients.Add(
+                db.Ingredients.First(i => i.Name == "Butter")
+            );
+            db.SaveChanges();
+
+            // make meat pie by slightly adjusting apple pie ingredients
+            var meatPie = db.Dishes.First(d => d.ID == 03);
+            foreach (Ingredient i in applePie.Ingredients)
             {
-                
-                Console.WriteLine("Inserting a salad into dishes table");
-                db.Add(new Dish { Name = "Salad", ID=01});
+                meatPie.Ingredients.Add(i);
+            };
+            meatPie.Ingredients.Remove(db.Ingredients.First(i => i.Name == "Apple"));
+            meatPie.Ingredients.Add(db.Ingredients.First(i => i.Name == "Beef"));
+            meatPie.Name = "Grandma's Meat Pie"; // change name of meat pie
 
-                Console.WriteLine("Inserting 5 different ingredients into ingredient table");
-                db.Add(new Ingredient { Name = "Apple" });
-                db.Add(new Ingredient { Name = "Sugar" });
-                db.Add(new Ingredient { Name = "Flour", HasGluten = true });
-                db.Add(new Ingredient { Name = "Butter", HasDairy = true });
-                db.Add(new Ingredient { Name = "Beef", IsMeat = true});
+            db.SaveChanges();
 
-                
-                Console.WriteLine("Inserting apple and meat pies into dishes table");
-                db.Add(new Dish { Name = "Apple Pie", ID = 02 });
-                db.Add(new Dish { Name = "Meat Pie", ID = 03 });
-
-                db.SaveChanges();
-
-                var applePie = db.Dishes.First(d => d.ID == 02);
-                // store apple pie ingredients by retrieving them from ingredients db
-                Console.WriteLine("Storing the ingredients for dish");
-                applePie.Ingredients.Add(
-                    db.Ingredients.First(i => i.Name == "Apple") 
-                    );
-                applePie.Ingredients.Add(
-                    db.Ingredients.First(i => i.Name == "Sugar")
-                    );
-                applePie.Ingredients.Add(
-                    db.Ingredients.First(i => i.Name == "Flour")
-                );
-                applePie.Ingredients.Add(
-                    db.Ingredients.First(i => i.Name == "Butter")
-                );
-                db.SaveChanges();
-
-                // make meat pie by slightly adjusting apple pie ingredients
-                var meatPie = db.Dishes.First(d => d.ID == 03);
-                foreach (Ingredient i in applePie.Ingredients)
-                {
-                    meatPie.Ingredients.Add(i);
-                };
-                meatPie.Ingredients.Remove(db.Ingredients.First(i => i.Name == "Apple"));
-                meatPie.Ingredients.Add(db.Ingredients.First(i => i.Name == "Beef"));
-                meatPie.Name = "Grandma's Meat Pie"; // change name of meat pie
-
-                db.SaveChanges();
-
-                // Delete
-                Console.WriteLine("Deleting salad dish from menu");
-                db.Remove(db.Dishes.First(d => d.ID == 01));
-                db.SaveChanges();
-
-            }
+            // Delete
+            Console.WriteLine("Deleting salad dish from menu");
+            db.Remove(db.Dishes.First(d => d.ID == 01));
+            db.SaveChanges();
         }
     }
 }
