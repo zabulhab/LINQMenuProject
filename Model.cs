0583cd565a7275ca498f978d8b6a525ca93345c7
Zena Abulhab <zabulhab@umich.edu>
2020-10-27 01:46:56 -0400

    Initial commit

---
+++ Model.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFTest
+{
+    public class MenuContext : DbContext
+    {
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=Recipes.db");
+        //public MenuContext(DbContextOptions<MenuContext> options): base(options)
+        //{
+        //    Database.EnsureCreated();
+        //}
+    }
+
+    // Food composed of ingredients
+    public class Dish
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        // Ingredients in this recipe
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+    }
+
+    public class Ingredient
+    {
+        [Key] // f
+        public string Name { get; set; } // unique identifier
+
+        public bool IsMeat { get; set; } = false;
+        public bool IsNut { get; set; } = false;
+        public bool HasDairy { get; set; } = false;
+        public bool HasGluten { get; set; } = false;
+        public bool IsSpicy { get; set; } = false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+// My first time using the Entity Framework with LINQ queries
+namespace EFTest
+{
+    // Makes a new person context, into which you can add people to the people
+    // table and meals to another table
+    class Program
+    {
+        static void Main()
+        {
+            using (var db = new MenuContext())
+            {
+                
+                Console.WriteLine("Inserting a salad into dishes table");
+                db.Add(new Dish { Name = "Salad", ID=01});
+
+                Console.WriteLine("Inserting 5 different ingredients into ingredient table");
+                db.Add(new Ingredient { Name = "Apple" });
+                db.Add(new Ingredient { Name = "Sugar" });
+                db.Add(new Ingredient { Name = "Flour", HasGluten = true });
+                db.Add(new Ingredient { Name = "Butter", HasDairy = true });
+                db.Add(new Ingredient { Name = "Beef", IsMeat = true});
+
+                
+                Console.WriteLine("Inserting apple and meat pies into dishes table");
+                db.Add(new Dish { Name = "Apple Pie", ID = 02 });
+                db.Add(new Dish { Name = "Meat Pie", ID = 03 });
+
+                db.SaveChanges();
+
+                var applePie = db.Dishes.First(d => d.ID == 02);
+                // store apple pie ingredients by retrieving them from ingredients db
+                Console.WriteLine("Storing the ingredients for dish");
+                applePie.Ingredients.Add(
+                    db.Ingredients.First(i => i.Name == "Apple") 
+                    );
+                applePie.Ingredients.Add(
+                    db.Ingredients.First(i => i.Name == "Sugar")
+                    );
+                applePie.Ingredients.Add(
+                    db.Ingredients.First(i => i.Name == "Flour")
+                );
+                applePie.Ingredients.Add(
+                    db.Ingredients.First(i => i.Name == "Butter")
+                );
+                db.SaveChanges();
+
+                // make meat pie by slightly adjusting apple pie ingredients
+                var meatPie = db.Dishes.First(d => d.ID == 03);
+                foreach (Ingredient i in applePie.Ingredients)
+                {
+                    meatPie.Ingredients.Add(i);
+                };
+                meatPie.Ingredients.Remove(db.Ingredients.First(i => i.Name == "Apple"));
+                meatPie.Ingredients.Add(db.Ingredients.First(i => i.Name == "Beef"));
+                meatPie.Name = "Grandma's Meat Pie"; // change name of meat pie
+
+                db.SaveChanges();
+
+                // Delete
+                Console.WriteLine("Deleting salad dish from menu");
+                db.Remove(db.Dishes.First(d => d.ID == 01));
+                db.SaveChanges();
+
+            }
+        }
+    }
+}
